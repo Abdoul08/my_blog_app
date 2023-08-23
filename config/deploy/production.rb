f8c014c0c6be26eee2ce1b120aed52a40cd30c45
Abdoul08 <diawarabdoulaye55@gmail.com>
2023-08-23 07:40:13 +0200

    add nginx and unicorn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -53,7 +53,7 @@
    user: "ec2-user",
    roles: %w{web app},
    ssh_options: {
-     user: "user_name", # overrides user setting above
+     user: "ec2-user", # overrides user setting above
      keys: %w(/home/abdoul/.ssh/dic_sample.pem),
      forward_agent: false,
      auth_methods: %w(publickey password)
