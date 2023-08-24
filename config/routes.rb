df434c44877b80d4b7c72368edb863473a4d715d
Abdoul08 <diawarabdoulaye55@gmail.com>
2023-08-24 02:28:25 +0200

    backend

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#index'
-  resources :categorie
+  resources :categories
   resources :users, except: [ :new ]
   resources :favorites, only: [:create, :destroy]
   resources :articles
